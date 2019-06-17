0a27b1d8cbd081dc147f0d7b97312e341ee6f116
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-17 01:11:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,16 +6,11 @@ class Dog
   def name
     @name
   end
-    name=dog_name
-    @this_doggies_name = dog_name
+   
+  def name=(new_name)
+    @name = new_name
   end
- 
-  def name
-    @this_doggies_name
-  end
-  
-  def dog_name=(my_name)
-    @this_doggies_name = my_name 
-  end
-  
 end
+   
+   
+   
