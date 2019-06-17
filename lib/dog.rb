d295cf7d66998c6e150524332841078d6481e842
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-17 01:16:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,22 @@
 class Dog 
   def initialize(dog_name)
-    @name=dog_name
+    @name = dog_name
   end
   
   def dog_name
     @name
   end
-   
-  def dog_name=(new_name)
-    @name = new_name
-  end
 end
    
+  
    
    
+#   class Person
+#   def initialize(name)
+#     @name = name
+#   end
+ 
+#   def name
+#     @name
+#   end
+# end
